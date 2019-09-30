4f6cd9b53b4301616edaa1b8a584f8d6264ff4cd
Вадим Мишхожев <vadim.stug@gmail.com>
2019-09-30 22:16:07 +0700

    Init project

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * lustshop Theme Customizer
+ *
+ * @package lustshop
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function lustshop_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'lustshop_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'lustshop_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'lustshop_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function lustshop_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function lustshop_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function lustshop_customize_preview_js() {
+	wp_enqueue_script( 'lustshop-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'lustshop_customize_preview_js' );
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wp theme activate lustshop --allow-root
+wp plugin install loco-translate --activate --allow-root
