9bb5a0b11cce7875786695c842c4b0ece690c6fc
Вадим Мишхожев <vadim.stug@gmail.com>
2019-10-01 11:35:11 +0700

    Добавлен плагин для синхронизации темы и плагинов (#2)

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 wp theme activate lustshop --allow-root
 wp plugin install loco-translate --activate --allow-root
+wp plugin install https://github.com/afragen/github-updater/archive/develop.zip --activate --allow-root
