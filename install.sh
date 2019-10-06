b8ae6dd047e040f6025b74b84ec4c1a878704e26
Вадим Мишхожев <vadim.stug@gmail.com>
2019-10-06 22:16:31 +0700

    Добавлены все плагины с сервера разработки (#16)

--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 wp theme activate lustshop --allow-root
+wp plugin install contact-form-7 --activate --allow-root
+wp plugin install contact-form-7-to-database-extension --activate --allow-root
+wp plugin install cyr3lat --activate --allow-root
+wp plugin install stops-core-theme-and-plugin-updates --activate --allow-root
+wp plugin install de-updraftplus-backup-exclude-image-thumbnails --activate --allow-root
+wp plugin install post-thumbnail-editor --activate --allow-root
 wp plugin install loco-translate --activate --allow-root
-wp plugin install cyr2lat --activate --allow-root
+wp plugin install redirection --activate --allow-root
+wp plugin install updraftplus --activate --allow-root
+wp plugin install user-role-editor --activate --allow-root
 wp plugin install woocommerce --activate --allow-root
+wp plugin install woocommerce-checkout-manager --activate --allow-root
+wp plugin install woocommerce-xml-csv-product-import --activate --allow-root
+wp plugin install yith-woocommerce-compare --activate --allow-root
+wp plugin install yith-woocommerce-wishlist --activate --allow-root
+wp plugin install yith-woocommerce-zoom-magnifier --activate --allow-root
+wp plugin install wordpress-seo --activate --allow-root
+wp plugin install yandex-mail --activate --allow-root
+wp plugin install yandexkassa --activate --allow-root
 wp plugin install imagify --activate --allow-root
-# wp plugin install https://github.com/afragen/github-updater/archive/develop.zip --activate --allow-root
+wp plugin install https://github.com/wp-sync-db/wp-sync-db/archive/master.zip --activate --allow-root
+wp plugin install https://github.com/wp-sync-db/wp-sync-db-media-files/archive/master.zip --activate --allow-root
