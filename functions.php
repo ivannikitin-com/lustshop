32367e242339c8466329b71f7799cf11354cb833
Вадим Мишхожев <vadim.stug@gmail.com>
2019-10-09 22:24:00 +0700

    Еще одна проверка на отключение стилей плагина

--- functions.php
+++ functions.php
@@ -221,4 +221,8 @@ require get_template_directory() . '/inc/optimize.php';
  */
 require get_template_directory() . '/inc/custom-function.php';
 
-wp_dequeue_style('yith-wcwl-font-awesome');
+add_action( 'wp_print_styles', 'deregister_my_styles', 100 );
+ 
+function deregister_my_styles() {
+	wp_deregister_style( 'yith-wcwl-font-awesome' );
+}
