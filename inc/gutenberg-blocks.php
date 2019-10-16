a7c1117b0718621696e1199dfa25653d70a251e0
Вадим Мишхожев <vadim.stug@gmail.com>
2019-10-16 13:11:58 +0700

    Правка стилей для блока "Категорий товаров"

--- inc/gutenberg-blocks.php
+++ inc/gutenberg-blocks.php
@@ -9,11 +9,35 @@ add_action( 'wp_enqueue_scripts', function() {
     $version
   );
 
+  wp_register_style( 'lustshop/woo-categories', 
+    get_template_directory_uri() . '/dist/css/blocks/woo-categories/frontend.css', 
+    null,
+    $version
+  );
+
+  wp_register_style( 'lustshop/woo-category', 
+    get_template_directory_uri() . '/dist/css/blocks/woo-category/frontend.css', 
+    null,
+    $version
+  );
+
 } ); 
 
 add_filter( 'render_block', function($block_content, $block) {
   
 	if ( 'lustshop/blog' === $block['blockName'] ) {
+		ob_start();
+		wp_print_styles( $block['blockName'] );
+		$block_content = ob_get_clean() . $block_content;
+	}
+  
+	if ( 'lustshop/woo-categories' === $block['blockName'] ) {
+		ob_start();
+		wp_print_styles( $block['blockName'] );
+		$block_content = ob_get_clean() . $block_content;
+	}
+  
+	if ( 'lustshop/woo-category' === $block['blockName'] ) {
 		ob_start();
 		wp_print_styles( $block['blockName'] );
 		$block_content = ob_get_clean() . $block_content;
