8be3822e90ecbc810553e39fad0da9360f4a4b52
Груздева Светлана <gruzdeva.s@outlook.com>
2020-04-29 07:22:24 +0300

    Блок описания категории #66 (#78)
    * Задача выполнена
    
    * Файлы перенесены в faq-category
    
    Co-authored-by: Vadim Mishkhozhev <vadim.stug@gmail.com>

--- typography.php
+++ typography.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Template Name: page-typography
+ * Template name: typography
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -14,58 +14,30 @@
 
 get_header();
 ?>
+
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
 
-		<h2>Баннер для страницы категорий</h2>
-		<div class="archive-banner"
-			style="background: #1d1f26 url(//localhost:3000/wp-content/themes/lustshop/src/img/category.png) top left no-repeat; background-size: cover;">
-			<div class="archive-banner__info">
-				<div class="archive-banner__subtitle">Эротическое белье</div>
-				<div class="archive-banner__title">Dolce Piccante</div>
+		<div class="container-fluid">
 
-				<div class="archive-banner__button-wrap"><a href="#"
-						class="wp-block-lustshop-button wp-block-lustshop-button--secondary archive-banner__button">Выбрать
-						<svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12">
-							<path data-name="-e-arrow-white"
-								d="M6.99 6a1.011 1.011 0 0 1-.29.706l-4.99 5A1 1 0 1 1 .3 10.294L4.58 6 .3 1.706A1 1 0 1 1 1.71.294l4.99 5A1.011 1.011 0 0 1 6.99 6z"
-								fill="#fff" fill-rule="evenodd"></path>
-						</svg></a></div>
-			</div>
+
+
 		</div>
-		<?php
-		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-		<header>
-			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-		</header>
 		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
 			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/page/front-page' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+		endwhile; // End of the loop.
 		?>
-
+		<?php get_template_part( 'category', '' ); ?>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
