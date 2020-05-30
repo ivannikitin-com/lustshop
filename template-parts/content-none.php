cfb0dc589f8056168af81e21d704993c0c891f74
stug111 <stug111@users.noreply.github.com>
2020-05-30 05:22:23 +0000

    deploy: 35c99e856b8d574d5a97f00238f81a2153228924

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -36,14 +36,10 @@
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'lustshop' ); ?></p>
 			<?php
-			get_search_form();
-
 		else :
 			?>
-
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'lustshop' ); ?></p>
 			<?php
-			get_search_form();
 
 		endif;
 		?>
