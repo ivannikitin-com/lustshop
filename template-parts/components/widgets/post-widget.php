90b887db99de081d241d9cfeb5f3d8e53743a493
Груздева Светлана <gruzdeva.s@outlook.com>
2020-06-02 18:27:28 +0300

    Правки сделаны

--- template-parts/components/widgets/post-widget.php
+++ template-parts/components/widgets/post-widget.php
@@ -7,9 +7,9 @@
 ?>
 
 <article>
-    <a class="post-widget" href="<?php echo get_permalink($id); ?>">
-        <?php echo get_the_post_thumbnail($id, 'lustshop-widget-post'); ?>
-        <div class="post-widget__title"><?php echo get_the_title($id); ?></div>
-        <time class="post-widget__time"><?php echo get_the_time('d.m.Y', $id); ?></time>
-    </a>
+	<a class="post-widget" href="<?php echo get_permalink($id); ?>">
+		<?php echo get_the_post_thumbnail($id, 'lustshop-widget-post', array('class' => 'post-widget__image')); ?>
+		<div class="post-widget__title"><?php echo get_the_title($id); ?></div>
+		<time class="post-widget__time"><?php echo get_the_time('d.m.Y', $id); ?></time>
+	</a>
 </article>
